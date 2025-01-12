faf36be8f68c96bbad44c95059de6fa24dda6107
Shadow <bartekkolosowski91@gmail.com>
2025-01-13 00:29:32 +0100

    .

--- actions/giveaway_end_MOD.js
+++ actions/giveaway_end_MOD.js
@@ -1,25 +1,7 @@
 module.exports = {
-  //---------------------------------------------------------------------
-  // Action Name
-  //
-  // This is the name of the action displayed in the editor.
-  //---------------------------------------------------------------------
-
   name: "Giveaway END",
 
-  //---------------------------------------------------------------------
-  // Action Section
-  //
-  // This is the section the action will fall into.
-  //---------------------------------------------------------------------
-
   section: "Giveaway",
-
-  //---------------------------------------------------------------------
-  // Action Subtitle
-  //
-  // This function generates the subtitle displayed next to the name.
-  //---------------------------------------------------------------------
 
   subtitle(data, presets) {
     return `Ending the giveaway`;
@@ -32,16 +14,6 @@ module.exports = {
     return [data.varName, dataType];
   },
 
-  //---------------------------------------------------------------------
-  // Action Meta Data
-  //
-  // Helps check for updates and provides info if a custom mod.
-  // If this is a third-party mod, please set "author" and "authorUrl".
-  //
-  // It's highly recommended "preciseCheck" is set to false for third-party mods.
-  // This will make it so the patch version (0.0.X) is not checked.
-  //---------------------------------------------------------------------
-
   meta: {
     version: "2.1.9",
     preciseCheck: true,
@@ -50,163 +22,62 @@ module.exports = {
     downloadUrl: null,
   },
 
-  //---------------------------------------------------------------------
-  // Action Fields
-  //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
-  //---------------------------------------------------------------------
-
   fields: ["call", "storage", "varName"],
-
-  //---------------------------------------------------------------------
-  // Command HTML
-  //
-  // This function returns a string containing the HTML used for
-  // editing actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
-  //---------------------------------------------------------------------
 
   html(isEvent, data) {
     return `
-    <div class="dbmmodsbr1" style="height: 59px;">
-    <p>Mod Info:</p>
-    <p>Created by Shadow</p>
-    <p>Help: <a href="https://discord.gg/9HYB4n3Dz4" target="_blank" style="color: #00aaff; text-decoration: none;">discord</a></p>
-  </div>
-  
-  <style>
-  .dbmmodsbr1 {
-    position: absolute;
-    bottom: 0px;
-    border: 2px solid rgba(50, 50, 50, 0.7);
-    background: rgba(0, 0, 0, 0.7);
-    color: #999;
-    padding: 5px;
-    font-size: 12px;
-    left: 0px;
-    z-index: 999999;
-    cursor: default;
-    line-height: 1.2;
-    border-radius: 8px;
-    height: 59px;
-    width: auto;
-    transition: transform 0.3s ease, background-color 0.6s ease, color 0.6s ease;
-  }
-  .dbmmodsbr1:hover {
-    transform: scale(1.01);
-    background-color: rgba(29, 29, 29, 0.9);
-    color: #fff;
-  }
-  .dbmmodsbr1 p {
-    margin: 0;
-    padding: 0;
-  }
-  .dbmmodsbr1 a {
-    font-size: 12px;
-    color: #00aaff;
-    text-decoration: none;
-  }
-  .dbmmodsbr1 a:hover {
-    text-decoration: underline;
-  }
-  </style>
-  
-  
-  
-  <div
-    class="dbmmodsbr2"
-    data-url="https://github.com/Shadow64gg/DBM"
-    onclick="openExternalLink(event, 'https://github.com/Shadow64gg/DBM')"
-  >
-    <p>Mod Version:</p>
-    <p>1.0</p>
-  </div>
-  
-  <style>
-  .dbmmodsbr2 {
-    position: absolute;
-    bottom: 0px;
-    right: 0px;
-    border: 0px solid rgba(50, 50, 50, 0.7);
-    background: rgba(0, 0, 0, 0.7);
-    color: #999;
-    padding: 5px;
-    font-size: 12px;
-    z-index: 999999;
-    cursor: pointer;
-    line-height: 1.2;
-    border-radius: 8px;
-    text-align: center;
-    height: auto;
-    transition: transform 0.3s ease, background-color 0.6s ease, color 0.6s ease;
-  }
-  
-  .dbmmodsbr2:hover {
-    transform: scale(1.01);
-    background-color: rgba(29, 29, 29, 0.9);
-    color: #fff;
-  }
-  
-  .dbmmodsbr2 p {
-    margin: 0;
-    padding: 0;
-  }
-  </style>
-  
-  <script>
-  function openExternalLink(event, url) {
-    event.preventDefault();
-    window.open(url, "_blank");
-  }
-  </script>
-  </div><br>
-  <div style="float: left; width: calc(50% - 12px);">
-    <span class="dbminputlabel">Jump To Action</span>
-    <input id="call" class="round" type="number">
-   </div>
-   </div>
-   <div style="float: left; width: calc(100% - 12px);">
-    <br>
-    <store-in-variable dropdownLabel="Store Giveaway In" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
-   </div>
-  </div>
-  `;
+      <div style="float: left; width: calc(50% - 12px);">
+        <span class="dbminputlabel">Jump To Action</span>
+        <input id="call" class="round" type="number">
+      </div>
+      <div style="float: left; width: calc(100% - 12px);">
+        <br>
+        <store-in-variable dropdownLabel="Store Giveaway In" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
+      </div>
+    `;
   },
-
-  //---------------------------------------------------------------------
-  // Action Editor Init Code
-  //
-  // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
-  //---------------------------------------------------------------------
 
   init() {},
 
-  //---------------------------------------------------------------------
-  // Action Bot Function
-  //
-  // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existence.
-  //---------------------------------------------------------------------
-
   async action(cache) {
+    const {
+      existsSync,
+      writeFileSync,
+      readFileSync,
+      mkdirSync,
+    } = require("fs");
     const data = cache.actions[cache.index];
-    const giveaways = require("../data/giveaways.json");
-    const { writeFileSync } = require("fs");
-    const client = this.getDBM().Bot.bot;
+    const { Bot } = this.getDBM();
+    const client = Bot.bot;
+    const dataDir = "./data";
+    const giveawaysPath = `${dataDir}/giveaways.json`;
+
+    // Sprawdzenie i tworzenie katalogu, jeśli nie istnieje
+    if (!existsSync(dataDir)) {
+      mkdirSync(dataDir);
+    }
+
+    // Sprawdzenie i tworzenie pliku, jeśli nie istnieje
+    if (!existsSync(giveawaysPath)) {
+      const defaultContent = {};
+      writeFileSync(giveawaysPath, JSON.stringify(defaultContent, null, 2));
+    }
+
+    let giveaways;
+    try {
+      giveaways = JSON.parse(readFileSync(giveawaysPath, "utf8"));
+    } catch (error) {
+      console.error("Błąd podczas odczytu pliku giveaways.json:", error);
+      return this.callNextAction(cache);
+    }
 
     setInterval(() => {
       Object.keys(giveaways).forEach(async (giveawayid) => {
-        for (var i in giveaways[giveawayid]) {
-          const giveaway = giveaways[giveawayid].at(i);
-          if (new Date().getTime() > giveaway.end && giveaway.ended == false) {
+        const giveawayList = giveaways[giveawayid];
+        if (!Array.isArray(giveawayList)) return;
+
+        giveawayList.forEach(async (giveaway) => {
+          if (new Date().getTime() > giveaway.end && !giveaway.ended) {
             let winner = [];
 
             if (giveaway.members.length > 0) {
@@ -214,15 +85,12 @@ module.exports = {
                 winner.length < giveaway.winners &&
                 winner.length < giveaway.members.length
               ) {
-                for (o = 0; o < giveaway.winners; o++) {
-                  let winnerr =
-                    giveaway.members[
-                      Math.floor(Math.random() * giveaway.members.length)
-                    ];
-
-                  if (!winner.includes(winnerr)) {
-                    winner.push(winnerr);
-                  }
+                const winnerr =
+                  giveaway.members[
+                    Math.floor(Math.random() * giveaway.members.length)
+                  ];
+                if (!winner.includes(winnerr)) {
+                  winner.push(winnerr);
                 }
               }
             }
@@ -230,12 +98,16 @@ module.exports = {
             giveaway.ended = true;
             giveaway.end = new Date().getTime();
 
-            writeFileSync("./data/giveaways.json", JSON.stringify(giveaways));
+            writeFileSync(giveawaysPath, JSON.stringify(giveaways, null, 2));
+
             const gData = {};
-            gData.guild = client.guilds.cache.get(giveaway.guild);
+            const guild = client.guilds.cache.get(giveaway.guild);
+            if (!guild) return;
+
+            gData.guild = guild;
             gData.channel =
-              gData.guild.channels.cache.get(giveaway.channel) ||
-              (await gData.guild.channels.fetch(giveaway.channel));
+              guild.channels.cache.get(giveaway.channel) ||
+              (await guild.channels.fetch(giveaway.channel));
             gData.message =
               gData.channel.messages.cache.get(giveaway.msg) ||
               (await gData.channel.messages.fetch(giveaway.msg));
@@ -250,6 +122,7 @@ module.exports = {
               data.varName,
               cache
             );
+
             const val = parseInt(data.call, 10);
             const index = Math.max(val - 1, 0);
             if (cache.actions[index]) {
@@ -257,18 +130,10 @@ module.exports = {
               this.callNextAction(cache);
             }
           }
-        }
+        });
       });
     }, 1000);
   },
-  //---------------------------------------------------------------------
-  // Action Bot Mod
-  //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflicts between mods, be sure to alias
-  // functions you wish to overwrite.
-  //---------------------------------------------------------------------
 
   mod() {},
 };
